007eb7fefaad87f540ef4e409d8c3016558c37e6
Aar Rafi Shahriar Shad <aarrafishahriarshad@Aars-MacBook-Air.local>
2023-11-12 21:22:58 +0600

    Reduced redundancy

--- main.py
+++ main.py
@@ -6,34 +6,37 @@ print(logo)
 compare_A = random.choice(data)
 score = 0
 
+def answer_check(compare_A, compare_B):
+    """Checking the user input and generating an output accordingly"""
+
+    if compare_A['follower_count'] > compare_B['follower_count']:
+        return answer=="a"
+    else:
+        return answer=="b"
+
 while True:
     print(f"Compare A: {compare_A['name']}, a {compare_A['description']}, from {compare_A['country']}.")
     print(vs)
     compare_B = random.choice(data)
+
+    while compare_A == compare_B:
+        compare_B = random.choice(data)
+    
     print(f"Compare B: {compare_B['name']}, a {compare_B['description']}, from {compare_B['country']}.")
 
-    answer = input("Who has more followers? Type 'A' or 'B': ")
+    answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-    if answer == "A":
-        if compare_A['follower_count'] > compare_B['follower_count']:
-            score+=1
-            print(f"You're right! Current score: {score}")
-            compare_A = compare_B
-            
-        elif compare_A['follower_count'] < compare_B['follower_count']:
-            print(f"Sorry that's wrong! Final score: {score}")
-            break
+    while answer != "a" and answer != "b":
+        """Checking for an invalid answer."""
+        
+        answer = input("Invalid answer! Type 'A' or 'B': ").lower()
 
-    elif answer == "B":
-        if compare_B['follower_count'] > compare_A['follower_count']:
-            score+=1
-            print(f"You're right! Current score: {score}")
-            compare_A = compare_B
+    is_correct = answer_check(compare_A, compare_B)
 
-        elif compare_B['follower_count'] < compare_A['follower_count']:
-            print(f"Sorry that's wrong! Final score: {score}")
-            break
-
+    if is_correct == True:
+        score+=1
+        print(f"You're right! Current score: {score}")
+        compare_A = compare_B
     else:
-        print("Invalid answer!")
-        break
+        print(f"Sorry that's wrong! Final score: {score}")
+        break 
